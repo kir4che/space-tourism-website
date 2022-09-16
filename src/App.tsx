9d59486f2c6fe3bcb19b87e346b3060927687bca
kir4che <mollydcxxiii@gmail.com>
2022-09-16 19:05:11 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,8 @@ import './style.css';
 import routeConfig from './routes'; 
 
 const App: React.FC = () => {
-  const element = useRoutes(routeConfig)
-
-  return <>
-    { element }
-  </>
+  const element = useRoutes(routeConfig);
+  return element;
 }
 
 export default App;
